f09e2b106c353a31284a3fbd5dc90924be474a0f
waxcatape <89343493+waxcatape@users.noreply.github.com>
2023-03-08 04:54:46 +0300

    Update sicp-piclang.scm

--- sicp-piclang.scm
+++ sicp-piclang.scm
@@ -2,69 +2,69 @@
   #:export (segments->painter
             segments->shape
             transform-painter
-            make-vect
-            xcor-vect
-            ycor-vect
-            add-vect
-            sub-vect
-            scale-vect
-            make-segment
-            start-segment
-            end-segment
+            make-vect-answer
+            xcor-vect-answer
+            ycor-vect-answer
+            add-vect-answer
+            sub-vect-answer
+            scale-vect-answer
+            make-segment-answer
+            start-segment-answer
+            end-segment-answer
             make-frame
-            origin-frame
-            edge1-frame
-            edge2-frame
+            origin-frame-answer
+            edge1-frame-answer
+            edge2-frame-answer
             paint-lines
-            wave
+            wave-answer
             flip-vert
-            flip-horiz
-            below
+            flip-horiz-answer
+            below-answer
             beside
-            up-split
+            up-split-answer
             right-split
             corner-split
             square-limit))
 
 (use-modules (pict))
 
-(define (make-vect x y)
+(define (make-vect-answer x y)
   ;; a vector is a list of two coordinates. An alternate representation would be a cons pair.
   (list x y))
 
-(define xcor-vect car)
+(define xcor-vect-answer car)
 
-(define ycor-vect cadr)
+(define ycor-vect-answer cadr)
 
-(define (add-vect v w)
-  (list (+ (xcor-vect v) (xcor-vect w))
-        (+ (ycor-vect v) (ycor-vect w))))
+(define (add-vect-answer v w)
+  (list (+ (xcor-vect-answer v) (xcor-vect-answer w))
+        (+ (ycor-vect-answer v) (ycor-vect-answer w))))
 
-(define (sub-vect v w)
-  (list (- (xcor-vect v) (xcor-vect w))
-        (- (ycor-vect v) (ycor-vect w))))
+(define (sub-vect-answer v w)
+  (list (- (xcor-vect-answer v) (xcor-vect-answer w))
+        (- (ycor-vect-answer v) (ycor-vect-answer w))))
 
-(define (scale-vect s v)
-  (list (* s (xcor-vect v))
-        (* s (ycor-vect v))))
+(define (scale-vect-answer s v)
+  (list (* s (xcor-vect-answer v))
+        (* s (ycor-vect-answer v))))
 
 (define (make-frame origin edge1 edge2)
   ;; a frame is a list of three vectors
   (list origin edge1 edge2))
 
-(define origin-frame car)
+(define origin-frame-answer car)
 
-(define edge1-frame cadr)
+(define edge1-frame-answer cadr)
 
-(define edge2-frame caddr)
+(define edge2-frame-answer caddr)
 
-(define (make-segment start end)
+(define (make-segment-answer start end)
   ;; a segment is a list of two vectors
   (list start end))
 
-(define start-segment car)
+(define start-segment-answer car)
 
-(define end-segment cadr)
+(define end-segment-answer cadr)
 
 (define segments->shape list)
 
@@ -73,20 +73,20 @@
 (define (frame-coord-map frame)
   ;; Returns a function for adjusting a frame by a vector 
   (lambda (v)
-    (add-vect
-     (origin-frame frame)
-     (add-vect 
-      (scale-vect (xcor-vect v)
-                  (edge1-frame frame))
-      (scale-vect (ycor-vect v)
-                  (edge2-frame frame))))))
+    (add-vect-answer
+     (origin-frame-answer frame)
+     (add-vect-answer 
+      (scale-vect-answer (xcor-vect-answer v)
+                         (edge1-frame-answer frame))
+      (scale-vect-answer (ycor-vect-answer v)
+                         (edge2-frame-answer frame))))))
 
 (define (draw-line start end)
   ;; take two vectors, returns a line SVG object for pict
-  (line (xcor-vect start)
-        (ycor-vect start)
-        (xcor-vect end)
-        (ycor-vect end)))
+  (line (xcor-vect-answer start)
+        (ycor-vect-answer start)
+        (xcor-vect-answer end)
+        (ycor-vect-answer end)))
 
 (define (segments->painter segment-list)
   ;; takes a list of segments, returns a "painter" lambda, which applies a frame
@@ -97,9 +97,9 @@
      (lambda (segment)
        (draw-line
         ((frame-coord-map frame)
-         (start-segment segment))
+         (start-segment-answer segment))
         ((frame-coord-map frame)
-         (end-segment segment))))
+         (end-segment-answer segment))))
      segment-list)))
 
 
@@ -114,136 +114,136 @@
     (let* ((m (frame-coord-map frame))
            (new-origin (m origin)))
       (painter (make-frame new-origin
-                           (sub-vect (m corner1) new-origin)
-                           (sub-vect (m corner2) new-origin))))))
+                           (sub-vect-answer (m corner1) new-origin)
+                           (sub-vect-answer (m corner2) new-origin))))))
 
 (define (paint-lines painter)
   ;; use pict to compile an SVG with the elements described by painter
-  (let ((Frame (make-frame (make-vect 0 0)
-                           (make-vect 500 0)
-                           (make-vect 0 500))))
+  (let ((Frame (make-frame (make-vect-answer 0 0)
+                           (make-vect-answer 500 0)
+                           (make-vect-answer 0 500))))
     (apply lt-superimpose
            (painter Frame))))
 
 (define (flip-vert painter)
   (transform-painter painter
-                     (make-vect 0.0 1.0)  ;; new origin
-                     (make-vect 1.0 1.0)  ;; new end of edge1
-                     (make-vect 0.0 0.0)));; new end of edge2
+                     (make-vect-answer 0.0 1.0)  ;; new origin
+                     (make-vect-answer 1.0 1.0)  ;; new end of edge1
+                     (make-vect-answer 0.0 0.0)));; new end of edge2
 
-(define (flip-horiz painter)
+(define (flip-horiz-answer painter)
   (transform-painter painter
-                     (make-vect 1.0 0.0)
-                     (make-vect 0.0 0.0)
-                     (make-vect 1.0 1.0)))
+                     (make-vect-answer 1.0 0.0)
+                     (make-vect-answer 0.0 0.0)
+                     (make-vect-answer 1.0 1.0)))
 
 (define (beside painter1 painter2)
-  (let* ((split-point (make-vect 0.5 0.0))
+  (let* ((split-point (make-vect-answer 0.5 0.0))
           (paint-left  (transform-painter
                         painter1
-                        (make-vect 0.0 0.0)
+                        (make-vect-answer 0.0 0.0)
                         split-point
-                        (make-vect 0.0 1.0)))
+                        (make-vect-answer 0.0 1.0)))
           (paint-right (transform-painter
                         painter2
                         split-point
-                        (make-vect 1.0 0.0)
-                        (make-vect 0.5 1.0))))
+                        (make-vect-answer 1.0 0.0)
+                        (make-vect-answer 0.5 1.0))))
     (lambda (frame)
       (superimpose-shapes ;; <- the key change
        (paint-left frame)
        (paint-right frame)))))
 
-(define (below painter1 painter2)
-  (let* ((split-point (make-vect 0.0 0.5))
-         (paint-below (transform-painter painter1
+(define (below-answer painter1 painter2)
+  (let* ((split-point (make-vect-answer 0.0 0.5))
+         (paint-below-answer (transform-painter painter1
                                          split-point
-                                         (make-vect 1.0 0.5)
-                                         (make-vect 0.0 1.0)))
+                                         (make-vect-answer 1.0 0.5)
+                                         (make-vect-answer 0.0 1.0)))
          (paint-above (transform-painter painter2
-                                         (make-vect 0.0 0.0)
-                                         (make-vect 1.0 0.0)
+                                         (make-vect-answer 0.0 0.0)
+                                         (make-vect-answer 1.0 0.0)
                                          split-point)))
     (lambda (frame)
       (superimpose-shapes
        (paint-above frame)
-       (paint-below frame)))))
+       (paint-below-answer frame)))))
 
 
 (define (right-split painter n)
   (if (= n 0)
       painter
       (let ((smaller (right-split painter (- n 1))))
-        (beside painter (below smaller smaller)))))
+        (beside painter (below-answer smaller smaller)))))
 
-(define (up-split painter n)
+(define (up-split-answer painter n)
   (if (= n 0)
       painter
-      (let ((smaller (up-split painter (- n 1))))
-        (below painter (beside smaller smaller)))))
+      (let ((smaller (up-split-answer painter (- n 1))))
+        (below-answer painter (beside smaller smaller)))))
 
 (define (corner-split painter n)
   (if (= n 0)
       painter
-      (let* ((up (up-split painter (- n 1)))
+      (let* ((up (up-split-answer painter (- n 1)))
              (right (right-split painter (- n 1)))
              (top-left (beside up up))
-             (bottom-right (below right right))
+             (bottom-right (below-answer right right))
              (corner (corner-split painter (- n 1))))
-        (beside (below painter top-left)
-                (below bottom-right corner)))))
+        (beside (below-answer painter top-left)
+                (below-answer bottom-right corner)))))
 
 (define (square-limit painter n)
   (let* ((quarter (corner-split painter n))
-         (half (beside (flip-horiz quarter) quarter)))
-    (below (flip-vert half) half)))
+         (half (beside (flip-horiz-answer quarter) quarter)))
+    (below-answer (flip-vert half) half)))
 
-;; wave starts here
+;; wave-answer starts here
 
 (define legs
-  (segments->shape (make-segment (make-vect 0.35 0.65)
-                                 (make-vect 0.25 1))
-                   (make-segment (make-vect 0.65 0.65)
-                                 (make-vect 0.75 1))
-                   (make-segment (make-vect 0.3 1)
-                                 (make-vect 0.5 0.7))
-                   (make-segment (make-vect 0.7 1)
-                                 (make-vect 0.5 0.7))))
+  (segments->shape (make-segment-answer (make-vect-answer 0.35 0.65)
+                                        (make-vect-answer 0.25 1))
+                   (make-segment-answer (make-vect-answer 0.65 0.65)
+                                        (make-vect-answer 0.75 1))
+                   (make-segment-answer (make-vect-answer 0.3 1)
+                                        (make-vect-answer 0.5 0.7))
+                   (make-segment-answer (make-vect-answer 0.7 1)
+                                        (make-vect-answer 0.5 0.7))))
 
 (define left-arm
-  (segments->shape (make-segment (make-vect 0.45 0.2)
-                                 (make-vect 0.35 0.15))
-                   (make-segment (make-vect 0.35 0.15)
-                                 (make-vect 0.15 0.3))
-                   (make-segment (make-vect 0.15 0.3)
-                                 (make-vect 0 0.15))
-                   (make-segment (make-vect 0.4 0.3)
-                                 (make-vect 0.15 0.45))
-                   (make-segment (make-vect 0.15 0.45)
-                                 (make-vect 0 0.2))))
+  (segments->shape (make-segment-answer (make-vect-answer 0.45 0.2)
+                                        (make-vect-answer 0.35 0.15))
+                   (make-segment-answer (make-vect-answer 0.35 0.15)
+                                        (make-vect-answer 0.15 0.3))
+                   (make-segment-answer (make-vect-answer 0.15 0.3)
+                                        (make-vect-answer 0 0.15))
+                   (make-segment-answer (make-vect-answer 0.4 0.3)
+                                        (make-vect-answer 0.15 0.45))
+                   (make-segment-answer (make-vect-answer 0.15 0.45)
+                                        (make-vect-answer 0 0.2))))
 
 (define right-arm
-  (segments->shape (make-segment (make-vect 0.55 0.2)
-                                 (make-vect 0.65 0.15))
-                   (make-segment (make-vect 0.65 0.15)
-                                 (make-vect 1 0.55))
-                   (make-segment (make-vect 0.6 0.3)
-                                 (make-vect 1 0.65))))
+  (segments->shape (make-segment-answer (make-vect-answer 0.55 0.2)
+                                        (make-vect-answer 0.65 0.15))
+                   (make-segment-answer (make-vect-answer 0.65 0.15)
+                                        (make-vect-answer 1 0.55))
+                   (make-segment-answer (make-vect-answer 0.6 0.3)
+                                        (make-vect-answer 1 0.65))))
 
 (define torso
-  (segments->shape (make-segment (make-vect 0.4 0.3)
-                                 (make-vect 0.35 0.65))
-                   (make-segment (make-vect 0.6 0.3)
-                                 (make-vect 0.65 0.65))))
+  (segments->shape (make-segment-answer (make-vect-answer 0.4 0.3)
+                                        (make-vect-answer 0.35 0.65))
+                   (make-segment-answer (make-vect-answer 0.6 0.3)
+                                        (make-vect-answer 0.65 0.65))))
 (define head
-  (segments->shape (make-segment (make-vect 0.45 0)
-                                 (make-vect 0.4 0.1))
-                   (make-segment (make-vect 0.55 0)
-                                 (make-vect 0.6 0.1))
-                   (make-segment (make-vect 0.4 0.1)
-                                 (make-vect 0.45 0.2))
-                   (make-segment (make-vect 0.6 0.1)
-                                 (make-vect 0.55 0.2))))
+  (segments->shape (make-segment-answer (make-vect-answer 0.45 0)
+                                        (make-vect-answer 0.4 0.1))
+                   (make-segment-answer (make-vect-answer 0.55 0)
+                                        (make-vect-answer 0.6 0.1))
+                   (make-segment-answer (make-vect-answer 0.4 0.1)
+                                        (make-vect-answer 0.45 0.2))
+                   (make-segment-answer (make-vect-answer 0.6 0.1)
+                                        (make-vect-answer 0.55 0.2))))
 
-(define wave
+(define wave-answer
   (segments->painter (superimpose-shapes head torso legs right-arm left-arm)))
